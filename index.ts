92919e59b91dc04a8fb7376ce15acd619339a71d
Cristhian Fernandez <crisfervil@gmail.com>
2017-03-27 18:17:42 +0100

    Published the new DataTableSerializer class

--- index.ts
+++ index.ts
@@ -5,4 +5,6 @@ export * from "./src/Fetch";
 export * from "./src/Guid";
 export * from "./src/Messages";
 export * from "./src/DataTableSerializer";
+export * from "./src/SecurityUtil";
+
 
--- src/SecurityUtil.ts
+++ src/SecurityUtil.ts
@@ -1,18 +1,18 @@
 import { CRMClient } from './CRMClient';
 import { SetBusinessSystemUserRequest } from './Messages';
-import {EntityReference} from "./CRMDataTypes";
+import { EntityReference } from "./CRMDataTypes";
 
 
 export class SecurityUtil {
 
-    static SetUserBusinessUnit(crm:CRMClient, userId:string, businessUnitId:string){
-       
+    static SetUserBusinessUnit(crm: CRMClient, userId: string, businessUnitId: string) {
+
         var request = new SetBusinessSystemUserRequest();
         request.UserId = userId;
         request.BusinessId = businessUnitId;
-        request.ReassignPrincipal = new EntityReference(userId,'systemuser');
+        request.ReassignPrincipal = new EntityReference(userId, 'systemuser');
 
         crm.Execute(request);
     }
-    
+
 }
