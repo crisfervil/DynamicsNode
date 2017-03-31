60b7f93177527ee361809c9d253257cb67de54bc
Cristhian Fernandez <crisfervil@gmail.com>
2017-03-31 11:03:54 +0100

    Improved exception handling

--- src/cs/DynamicsNode/Startup.cs
+++ src/cs/DynamicsNode/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Reflection;
 
 namespace DynamicsNode
 {
@@ -8,62 +9,70 @@ namespace DynamicsNode
     {
         public async Task<object> Invoke(dynamic options)
         {
-            string connectionString = options.connectionString;
-            bool useFake = options.useFake;
-
-            CRMBridge bridge = new CRMBridge(connectionString, useFake);
-            return new
+            try
             {
-                Retrieve = (Func<object, Task<object>>)(
-                    async (i) =>
-                    {
-                        return bridge.Retrieve(i);
-                    }
-                ),
-                RetrieveMultiple = (Func<object, Task<object>>)(
-                    async (i) =>
-                    {
-                        return bridge.RetrieveMultiple((string)i);
-                    }
-                ),
-                Create = (Func<object, Task<object>>)(
-                    async (i) =>
-                    {
-                        return bridge.Create(i);
-                    }
-                ),
-                Update = (Func<object, Task<object>>)(
-                    async (i) =>
-                    {
-                        bridge.Update(i);
-                        return null;
-                    }
-                ),
-                Delete = (Func<object, Task<object>>)(
-                    async (i) =>
-                    {
-                        return bridge.Delete(i);
-                    }
-                ),
-                Associate = (Func<object, Task<object>>)(
-                    async (i) =>
-                    {
-                        return bridge.Associate(i);
-                    }
-                ),
-                Disassociate = (Func<object, Task<object>>)(
-                    async (i) =>
-                    {
-                        return bridge.Disassociate(i);
-                    }
-                ),
-                Execute = (Func<object, Task<object>>)(
-                    async (i) =>
-                    {
-                        return bridge.Execute(i);
-                    }
-                )
-            };
+                string connectionString = options.connectionString;
+                bool useFake = options.useFake;
+
+                CRMBridge bridge = new CRMBridge(connectionString, useFake);
+                return new
+                {
+                    Retrieve = (Func<object, Task<object>>)(
+                        async (i) =>
+                        {
+                            return bridge.Retrieve(i);
+                        }
+                    ),
+                    RetrieveMultiple = (Func<object, Task<object>>)(
+                        async (i) =>
+                        {
+                            return bridge.RetrieveMultiple((string)i);
+                        }
+                    ),
+                    Create = (Func<object, Task<object>>)(
+                        async (i) =>
+                        {
+                            return bridge.Create(i);
+                        }
+                    ),
+                    Update = (Func<object, Task<object>>)(
+                        async (i) =>
+                        {
+                            bridge.Update(i);
+                            return null;
+                        }
+                    ),
+                    Delete = (Func<object, Task<object>>)(
+                        async (i) =>
+                        {
+                            return bridge.Delete(i);
+                        }
+                    ),
+                    Associate = (Func<object, Task<object>>)(
+                        async (i) =>
+                        {
+                            return bridge.Associate(i);
+                        }
+                    ),
+                    Disassociate = (Func<object, Task<object>>)(
+                        async (i) =>
+                        {
+                            return bridge.Disassociate(i);
+                        }
+                    ),
+                    Execute = (Func<object, Task<object>>)(
+                        async (i) =>
+                        {
+                            return bridge.Execute(i);
+                        }
+                    )
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw ex;
+            }
         }
     }
 }
